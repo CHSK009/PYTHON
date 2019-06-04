a93c72c948ecb91dff966b43df5642d987c5b0a1
scznc <47009002+scznc@users.noreply.github.com>
2019-06-04 13:57:03 -0400

    icp1

---
+++ ICP 1/icp1.1.py
@@ -0,0 +1,7 @@
+b=int(input("enter first number"))
+c=int(input("enter second number"))
+print("addition",b+c)
+print("subtraction",b-c)
+print("multiplication",b*c)
+print("division",b/c)
+print("remainder",b%c)
---
+++ ICP 1/icp1.py
@@ -0,0 +1,14 @@
+def reverse(s):
+    str = ""
+    for i in s:
+        str = i + str
+    return str
+
+
+s = "PYTHON"
+
+
+print("The original string  is : ")
+print(s)
+print("The reversed string is : ")
+print(reverse(k))
